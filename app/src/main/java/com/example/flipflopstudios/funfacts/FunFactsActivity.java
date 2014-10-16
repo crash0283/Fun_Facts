a3d1135a7c45ae948581bf4f6dc65ebea0932320
crash0283 <chrisrasch@Chriss-MacBook-Pro.local>
2014-10-16 11:32:59 -0500

    Initial commit

---
+++ app/src/main/java/com/example/flipflopstudios/funfacts/FunFactsActivity.java
@@ -0,0 +1,49 @@
+package com.example.flipflopstudios.funfacts;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+
+
+public class FunFactsActivity extends Activity {
+
+    private FactBook mFactBook = new FactBook();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fun_facts);
+
+        final RelativeLayout relativeLayout = (RelativeLayout) findViewById(R.id.relativeLayout);
+
+        //Declare our view variables and assign them views from layout file
+        final TextView factLabel = (TextView) findViewById(R.id.factTextView);
+        Button showFactButton = (Button) findViewById(R.id.showFactButton);
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String fact = mFactBook.getFacts();
+                relativeLayout.setBackgroundColor(Color.RED);
+
+
+                //Update label with dynamic fact
+
+                factLabel.setText(fact);
+
+
+            }
+        };
+
+
+        showFactButton.setOnClickListener(listener);
+
+
+    }
+
+}
