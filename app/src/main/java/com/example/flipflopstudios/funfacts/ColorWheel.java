18981e6289cb021a0119c2d70aa39bb37a2bc4ae
chrisrasch <rasch83@gmail.com>
2014-10-16 11:40:15 -0500

    changed facts in ColorWheel class to colors

--- app/src/main/java/com/example/flipflopstudios/funfacts/ColorWheel.java
+++ app/src/main/java/com/example/flipflopstudios/funfacts/ColorWheel.java
@@ -8,17 +8,21 @@ import java.util.Random;
 public class ColorWheel {
 
     //Member variables (properties about the object)
-    public String[] mColors = {
-            "Ants stretch when they wake up in the morning.",
-            "Ostriches can run faster than horses.",
-            "Olympic gold medals are actually made mostly of silver.",
-            "You are born with 300 bones; by the time you are an adult you will have 206.",
-            "It takes about 8 minutes for light from the Sun to reach Earth.",
-            "Some bamboo plants can grow almost a meter in just one day.",
-            "The state of Florida is bigger than England.",
-            "Some penguins can leap 2-3 meters out of the water.",
-            "On average, it takes 66 days to form a new habit.",
-            "Mammoths still walked the earth when the Great Pyramid was being built." };
+    public String mColors[] = {
+            "#39add1", // light blue
+            "#3079ab", // dark blue
+            "#c25975", // mauve
+            "#e15258", // red
+            "#f9845b", // orange
+            "#838cc7", // lavender
+            "#7d669e", // purple
+            "#53bbb4", // aqua
+            "#51b46d", // green
+            "#e0ab18", // mustard
+            "#637a91", // dark gray
+            "#f092b0", // pink
+            "#b7c0c7"  // light gray
+    };
 
     //Method (things the object can do)
     public String getFacts() {
