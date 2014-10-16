d94fc6c956884fc57b45aea7f3ad3831ae5d72d0
chrisrasch <rasch83@gmail.com>
2014-10-16 14:27:37 -0500

    finished project

--- app/src/main/java/com/example/flipflopstudios/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/example/flipflopstudios/funfacts/FunFactsActivity.java
@@ -1,16 +1,18 @@
 package com.example.flipflopstudios.funfacts;
 
 import android.app.Activity;
-import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
-
+import android.widget.Toast;
 
 
 public class FunFactsActivity extends Activity {
+
+    public static final String TAG = FunFactsActivity.class.getSimpleName();
 
     private FactBook mFactBook = new FactBook();
     private ColorWheel mColorWheel = new ColorWheel();
@@ -48,6 +50,8 @@ public class FunFactsActivity extends Activity {
 
         showFactButton.setOnClickListener(listener);
 
+        //Toast.makeText(this,"Our activity was created!",Toast.LENGTH_LONG).show();
+        Log.d(TAG,"We're logging from the onCreate method");
 
     }
 
